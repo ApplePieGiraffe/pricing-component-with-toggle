15fc64e7dcdd30365df230240650ea02ca3e9981
ApplePieGiraffe <itsmejoshwoodson@gmail.com>
2020-11-06 13:56:16 +0300

    Wrap up project

--- js/script.js
+++ js/script.js
@@ -1,3 +1,26 @@
-const priceBasic = document.querySelector('#price-basic');
-const priceProf = document.querySelector('#price-prof');
-const priceMaster = document.querySelector('#price-master');
+const toggleBtn = document.querySelector('.toggle-switch__btn');
+const toggleCircle = document.querySelector('.toggle-switch__circle');
+const cardPrices = document.querySelectorAll('.pricing-card__price__number');
+
+let monthlyPrices = [19.99, 24.99, 39.99];
+let annualPrices = [119.99, 249.99, 399.99];
+
+function activateToggle() {
+    toggleCircle.classList.toggle('toggle-switch__circle--annual');
+    if (toggleCircle.classList.contains('toggle-switch__circle--annual')) {
+        cardPrices.forEach((price, index) => {
+            price.textContent = annualPrices[index];
+        });
+    } else {
+        cardPrices.forEach((price, index) => {
+            price.textContent = monthlyPrices[index];
+        });
+    }
+}
+
+toggleBtn.addEventListener('click', activateToggle);
+
+
+// const priceBasic = document.querySelector('#price-basic');
+// const priceProf = document.querySelector('#price-prof');
+// const priceMaster = document.querySelector('#price-master');
